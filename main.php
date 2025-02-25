11152350119ac5acbbe9bfc08911512652a0a232
xtophe38 <christophe.naud@gmail.com>
2025-02-25 14:52:21 +0100

    Rebuild file with the usage of a collection plan for LDAP.

--- main.php
+++ main.php
@@ -1,14 +1,6 @@
 <?php
-require_once(APPROOT.'collectors/LDAPCollector.class.inc.php');
-require_once(APPROOT.'collectors/iTopPersonLDAPCollector.class.inc.php');
-require_once(APPROOT.'collectors/iTopUserLDAPCollector.class.inc.php');
 
-Orchestrator::AddRequirement('1.0.0', 'ldap'); // LDAP support is required to run this collector
-
-$iRank = 1;
-Orchestrator::AddCollector($iRank++, iTopPersonLDAPCollector::class);
-
-if (Utils::GetConfigurationValue('collect_person_only', 'yes') !== 'yes')
-{
-	Orchestrator::AddCollector($iRank++, iTopUserLDAPCollector::class);
-}
+// Initialize collection plan
+require_once(APPROOT.'collectors/src/LDAPCollectionPlan.class.inc.php');
+require_once(APPROOT.'core/orchestrator.class.inc.php');
+Orchestrator::UseCollectionPlan('LDAPCollectionPlan');
